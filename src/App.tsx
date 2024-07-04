1e58683ea15f714ea9d4e49828fc3c1b9874e690
galeysh <galeysh@gmail.com>
2024-07-04 22:56:26 +0200

    del HW_06

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import "./App.css";
 import Lesson06 from "./lessons/lessons06/Lesoon06";
-import Homework_06 from "./homeworks/Homework_06/Homework_06";
 
 function App() {
-  return (
-    <div className="App">
-      {/* <Lesson06 /> */}
-      <Homework_06 />
-    </div>
-  );
+  return <div className="App">{/* <Lesson06 /> */}</div>;
 }
 
 export default App;
